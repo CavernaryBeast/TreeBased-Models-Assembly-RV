8102997eadbeeb284afbea4c78802f09454c047a
CavernaryBeast <raimundokarate98@gmail.com>
2019-06-21 00:20:16 +0200

    Training algorith implemented: Process, bootstrapping, Random Subspace Method, division of the data and fitting working. Also I have drawn the trees, but they are all the same, no matter if Bootstrapped and Random Subspace Method is applied

--- Functions/aux_functions.py
+++ Functions/aux_functions.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
+import random as random
+import sklearn
 from sklearn.tree import DecisionTreeClassifier
+from pandas import DataFrame
 
 def load_and_process_data(nombreArchivo):
 
@@ -29,6 +32,13 @@ def bootstrapping(train_df):
     
     return df_bootstrapped
 
+def aplicaBootstrapping(datosProcesados):
+    
+    datosTrasBootstrapping = sklearn.utils.resample(datosProcesados, replace=True)
+    
+    return datosTrasBootstrapping;
+
+
 def divide_data(df_bootstrapped):
     
     
@@ -42,16 +52,51 @@ def divide_data(df_bootstrapped):
     
     return examples,labels
 
+#   Function to implement the Random Subspace Method
+def random_subspace(bootstrapped_df, random_subspace):
+    
+    columns_chosen = {}
+    n_columns = bootstrapped_df.shape[1]
+    column_indices = list(range(n_columns - 1))    # Excluding the last column which is the label
+    n_columns_choosed = int(n_columns*random_subspace)
+    
+    if (random_subspace < 0) or (random_subspace > 1):
+        print('If you want the Random Subspace Method to work, you have to choose a number betweet 0 and 1')
+        
+    else:
+        column_indices = random.sample(population=column_indices, k=n_columns_choosed)
+    
+    for column_index in column_indices:          
+        values = bootstrapped_df.iloc[:, column_index]
+        #We can take only the unique values if we want, cause the bootstrapping can introduce some values that are equal
+        # unique_values = np.unique(values)
+        
+        #potential_splits[column_index] = unique_values
+        columns_chosen[column_index] = values
+    columns_chosen = DataFrame.from_dict(columns_chosen)
+    
+    return columns_chosen
+
     #def meta_algorithm(train_df, n_trees, n_features, dt_max_depth):
     
-def meta_algorithm(train_df, n_trees, n_features):
+def train_forest(file_name, n_trees, n_features, max_depth):
+    
+    processed_data = load_and_process_data(file_name)
+    
     forest = []
+    decisionTree = DecisionTreeClassifier(max_depth=max_depth)
     for i in range(n_trees):
-        df_bootstrapped = bootstrapping(train_df, len(train_df))
         
-       #tree = decisionTree.DecisionTreeClassifier(max_depth = dt_max_depth, )
-        
-        decisionTree = DecisionTreeClassifier()
+        #First, we apply the Bootstrapping
+        df_bootstrapped = aplicaBootstrapping(processed_data)
+        #Second, we divide the data into features(examples) and labels
+        examples, labels = divide_data(df_bootstrapped)
+        #Third, we execute the Random Subspace Method to the examples
+        examples_randomized = random_subspace(examples, n_features)
+        print(examples_randomized.head(10))
+        #Fourth, we execute the fitting
+        tree = decisionTree.fit(examples_randomized, labels)
+        #tree = decisionTree.DecisionTreeClassifier(max_depth = dt_max_depth, )
         forest.append(tree)
     
     return forest
